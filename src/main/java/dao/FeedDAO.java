c6a3aad18f6be69a77d73c2a86cc2976ced644d1
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-11-11 09:33:04 +0900

    hyunseo

--- src/main/java/dao/FeedDAO.java
+++ src/main/java/dao/FeedDAO.java
@@ -13,24 +13,96 @@ import org.json.simple.parser.ParseException;
 
 import util.ConnectionPool;
 
-public class UserDAO {	
-	public boolean insert(String uid, String jsonstr) throws NamingException, SQLException {
+public class FeedDAO {	
+	public boolean insert(String uid, String jsonstr) throws NamingException, SQLException, ParseException {
 		Connection conn = ConnectionPool.get();
 		PreparedStatement stmt = null; 
+		ResultSet rs = null;
 		try {
-			String sql = "INSERT INTO user(id, jsonstr) VALUES(?, ?)";
+			synchronized(this) {
+				
+			String sql = "SELECT no FROM feed ORDER BY no DESC LIMIT 1";
 			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, uid);
-			stmt.setString(2, jsonstr);
+			rs = stmt.executeQuery();
+			
+			int max = (!rs.next()) ? 0 : rs.getInt("no");
+			stmt.close(); rs.close();
+			
+			JSONParser parser = new JSONParser();
+			JSONObject jsonobj = (JSONObject) parser.parse(jsonstr);
+			jsonobj.put("no", max + 1);
+			jsonobj.put("id", uid);
+			
+			String sql2 = "INSERT INTO feed(no, id, jsonstr) VALUES(?, ?, ?)";
+			stmt = conn.prepareStatement(sql2);
+			stmt.setInt(1,  max + 1);
+			stmt.setString(2, uid);
+			stmt.setString(3, jsonobj.toJSONString());
 			
 			int count = stmt.executeUpdate();
 			return (count == 1) ? true : false;
+			}
 		} finally {
+			if(rs != null) rs.close();
 			if (stmt != null) stmt.close();
 			if (conn != null) conn.close();
 		}
 	}
 	
+	public String getFeedList(String maxNo) throws NamingException, SQLException {
+		Connection conn = ConnectionPool.get();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;	
+		try {
+			String sql = "select jsonstr from feed";
+			if(maxNo != null) {
+				sql += " where no < " + maxNo;
+			}
+			sql += " order by NO desc limit 3";
+			
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			String str = "[";
+			int cnt = 0;
+			while(rs.next()) {
+				if(cnt ++ > 0) str += ", ";
+				str += rs.getString("jsonstr");
+			}
+			return str + "]";
+			
+		}finally {
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			if(conn != null) conn.close();
+		}
+	}
+	
+	public String getFeedDetail(String no) throws NamingException, SQLException {
+		Connection conn = ConnectionPool.get();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;	
+		try {
+			String sql = "select * from feed where no = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, no);
+			rs = stmt.executeQuery();
+			
+			String str = "[";
+			int cnt = 0;
+			while(rs.next()) {
+				if(cnt ++ > 0) str += ", ";
+				str += rs.getString("jsonstr");
+			}
+			return str + "]";
+			
+		}finally {
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			if(conn != null) conn.close();
+		}
+	}
+//	공백
 	public boolean exists(String uid) throws NamingException, SQLException {
 		Connection conn = ConnectionPool.get();
 		PreparedStatement stmt = null;
@@ -130,27 +202,27 @@ public class UserDAO {
 		}
 	}
 	
-	public String myInfo(String uid) throws NamingException, SQLException {
+	public String getIdByFeedNo(String no) throws NamingException, SQLException {
 		Connection conn = ConnectionPool.get();
 		PreparedStatement stmt = null;
-		ResultSet rs = null;	
+		ResultSet rs = null;
 		try {
-			stmt = conn.prepareStatement("select jsonstr from user where id = ?");
-			stmt.setString(1, uid);
+			String sql = "SELECT id FROM feed where no = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, no);
 			rs = stmt.executeQuery();
-			
-			String str = "[";
+			String str = "";
 			int cnt = 0;
 			while(rs.next()) {
 				if(cnt ++ > 0) str += ", ";
-				str += rs.getString("jsonstr");
+				str += rs.getString("id");
 			}
-			return str + "]";
+			return str + "";
 			
-		}finally {
-			if(rs != null) rs.close();
-			if(stmt != null) stmt.close();
-			if(conn != null) conn.close();
+		} finally {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (conn != null) conn.close();
 		}
 	}
 }
